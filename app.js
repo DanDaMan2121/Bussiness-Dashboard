a46f435ddec1309c2d95bab256376b05a6ad53bf
Daniel Araujo <81410120+DanDaMan2121@users.noreply.github.com>
2025-11-18 13:55:28 -0500

    rework

--- app.js
+++ app.js
@@ -1,287 +1,150 @@
-import { getDatabase, ref, set, remove, onValue, child, get} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
-import {} from './firebase.js';
+import { writeUserData, removeUserData, readUserData, updateUserData } from './firebase.js';
 import { Pizza } from './pizzClass.js';
 
-const addButton = document.getElementById('addButton');
-const itemInput = document.getElementById('item');
-const menuContainer = document.getElementById('Other');
 
-const addPizzaButton = document.getElementById('addPizza');
-const pizzaInput = document.getElementById('itemPizza');
+// function createOption
+const menuContainer = document.getElementById('menuContainer');
+const otherContainer = document.getElementById('otherContainer');
 const pizzaContainer = document.getElementById('pizzaContainer');
+const ingredientContainer = document.getElementById('ingredientContainer');
 
-const addPizzMenuButton = document.getElementById('addPizzaMenu');
-const pizazMenuInput = document.getElementById('itemPizzaMenu');
-const pizzaMenuContainer = document.getElementById('pizzaMenu');
 
-async function writeUserData(parentId, childId) {
-  console.log('write ran');
-  console.log(`parentId: ${parentId}, childId: ${childId}`);
-  const db = getDatabase();
-  await set(ref(db, parentId + '/' + childId), '')
-  .then(() => {
-    console.log('data set successfully');
-    return true;
-  })
-  .catch ((error) => {
-    console.error(`Error setting node: ${error}`);
-    return false;
-  });
-}
+function addItemButton(container) {
+  let addItemDiv = document.createElement('div');
+  addItemDiv.className = 'addContainer';
 
-async function wirtePizzaData(parentId, childId,  myPizza) {
-  // console.log('pizzaWrite');
-  // const db = getDatabase();
-  // await set(ref(db, parentId + '/' + childId), '')
-  // .then(() => {
-  //   console.log('data set successfully');
-  //   return true;
-  // })
-  // .catch ((error) => {
-  //   console.error(`Error setting node: ${error}`);
-  //   return false;
-  // });
-  const startPath = parentId + '/' + childId;
-  console.log(myPizza);
-  readPizza(startPath, pizzaMenuContainer, myPizza);
-
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = 'add item';
   
 
-}
-
-function readPizza(startPath, container, myData) {
-for (const key in myData) {
-  if (key != 'PID' && key != 'quantity') {
-    createPizzaDiv(startPath, container, key);
-      const keyPath = startPath + '/' + key;
-      let keyDiv = document.getElementById(keyPath); // here is the problem // path name must be given
-      for (const item in myData[key]) {  
-          createItemDiv(item, keyDiv);
-          const itemPath = keyPath + '/' + item;
-          let itemDiv = document.getElementById(itemPath);
-          itemDiv.className = 'itemDiv';
-          // console.log(itemDiv)
-        for (const ingredient in myData[key][item]) {
-          createIngredientDiv(ingredient, itemDiv);
-        }
-      }
+  const addButton = document.createElement('button');
+  addButton.textContent = 'add';
+  addButton.addEventListener('click', () => {
+    const currentInput = input.value;
+    input.value = '';
+    if (currentInput != null && currentInput != '') {
+      addItemDiv.append(createItem('testDir', currentInput));
+      writeUserData('testDir',  currentInput); // new item new path
     }
-  }
-}
-
-function removeUserData(parentId, userId) {
-  console.log('remove ran');
-  console.log(`parentId: ${parentId}, userId: ${userId}`);
-  const db = getDatabase();
-  remove(ref(db, parentId + '/' + userId));
+    
+  })
+  addItemDiv.append(input, addButton);
+  container.append(addItemDiv);
 
 }
 
-async function readUserData(parentId, childId) { // a single snapshot of the data
-  const dbRef = ref(getDatabase());
-  let data = '';
-  await get(child(dbRef, parentId + '/' + childId)).then((snapshot) => {
-    if (snapshot.exists()){
-      data = snapshot.val();
-    } else {
-      console.log('No data available');
-    }
-  }).catch((error) => {
-    console.error(error);
-    // return false;
-  });
-  // console.log(myMenuItems);
-  return data;
-}
+function createItem(path, item) {
+  const currentPath = path + '/' + item;
+  let itemContainer = document.createElement('div');
+  
+  let itemSpan = document.createElement('span');
+  itemSpan.textContent = item;
 
-function createPizzaDiv(myKey, parentContainer, name) {
-  let pizzaContainer = document.createElement('div');
-  let modContainer = document.createElement('div');
-  let optionContainer = document.createElement('div');
-  // console.log(myKey);
-  optionContainer.id = myKey + '/' + name;
-  pizzaContainer.append(modContainer, optionContainer);
-  parentContainer.append(pizzaContainer);
-
-  let pizzaTitle = document.createElement('span');
-  pizzaTitle.textContent = name;
   let removeButton = document.createElement('button');
   removeButton.textContent = 'remove';
   removeButton.addEventListener('click', () => {
-      removeUserData(myKey, name);
-      while (pizzaContainer.firstChild) {
-        pizzaContainer.removeChild(pizzaContainer.firstChild);
-      }
-  });
-  modContainer.append(pizzaTitle, removeButton);
+    removeUserData(path, item);
+    itemContainer.remove();
+  })
+  
 
-  let optionInput = document.createElement('input');
-  optionInput.type = 'text';
-  optionInput.placeholder = 'add option';
+  let itemInput = document.createElement('input');
+  itemInput.type = 'text';
+  itemInput.placeholder = 'option';
+
   let addButton = document.createElement('button');
   addButton.textContent = 'add';
   addButton.addEventListener('click', () => {
-    const inputVal = optionInput.value;
+    const currentInput = itemInput.value;
+    itemInput.value = '';
+    if (currentInput != null && currentInput != '') {
+      itemContainer.append(createOption(currentPath, currentInput));
+      writeUserData(currentPath, currentInput);
+    }
+  });
+
+  itemContainer.append(itemSpan, removeButton, itemInput, addButton);
+  return itemContainer;
+}
+
+function createOption(path, option) {
+  const currentPath = path + '/' + option;
+  let optionContainer = document.createElement('div');
+
+  let optionSpan = document.createElement('span');
+  optionSpan.textContent = option;
+
+  let removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  removeButton.addEventListener('click', () => {
+    optionContainer.remove();
+    console.log(path);
+    removeUserData(path, option);
+  })
+
+  let optionInput = document.createElement('input');
+  optionInput.type = 'text';
+  optionInput.placeholder = 'ingredient';
+
+  let addButton = document.createElement('button');
+  addButton.textContent = 'add';
+  addButton.addEventListener('click', () => {
+    const currentInput = optionInput.value;
     optionInput.value = '';
-    if (inputVal != '' && inputVal != null){
-      const parentId = optionContainer.id
-      const result = writeUserData(parentId, inputVal);
-      if (result) {
-        createItemDiv(inputVal, optionContainer);
-      }
+    if (currentInput != null && currentInput != '') {
+      optionContainer.append(createIngredient(currentPath, currentInput));
+      writeUserData(currentPath, currentInput);
     }
   });
-  modContainer.append(optionInput, addButton);
+
+  optionContainer.append(optionSpan, removeButton, optionInput, addButton);
+
+  return optionContainer;
 }
 
-function createItemDiv(name, parentContainer) {
-  let itemContainer = document.createElement('div');
-  itemContainer.className = 'itemContainer';
-  let itemDiv = document.createElement('div');
-  let modContainer = document.createElement('div');
-  modContainer.className = 'modContainer';
-  let itemName = document.createElement('span');
-  let removeItem = document.createElement('button');
+function createIngredient(path, ingredient) {
+  const currentPath = path + '/' + ingredient;
+  let iContainer = document.createElement('div');
+  
+  let iSpan = document.createElement('span');
+  iSpan.textContent = ingredient;
 
-  // add ingredient
-  let inputIngedient = document.createElement('input');
-  inputIngedient.type = 'text';
-  inputIngedient.placeholder = 'add ingredient';
-  let addIngredient = document.createElement('button'); // WPI
-  addIngredient.textContent = 'add';
+  let removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  removeButton.addEventListener('click', () => {
+    iContainer.remove();
+    // removeUserData(path, ingredient);
+    updateUserData(path, ingredient, null);
+  })
 
-  itemName.textContent = name;
-  itemDiv.id = parentContainer.id + '/' + name; // parent path must be ID
-  removeItem.textContent = 'remove';
-
-  removeItem.addEventListener('click', () => {
-    removeUserData('Other', itemDiv.id);
-    while (itemContainer.firstChild) {
-      itemContainer.removeChild(itemContainer.firstChild);
-    }
-  });
-
-  addIngredient.addEventListener('click', () => {
-    const inputVal = inputIngedient.value;
-    inputIngedient.value = '';
-    if (inputVal != '' && inputVal != null){
-      const parentNode = itemDiv.id; // path name must be given
-      console.log(parentNode);
-      const result = writeUserData(parentNode, inputVal);
-      if (result) {
-        createIngredientDiv(inputVal, itemDiv);
-      }
-    }
-  });
-
-  modContainer.append(itemName, removeItem);
-  itemDiv.append(modContainer);
-  itemDiv.append(inputIngedient, addIngredient);
-  itemContainer.append(itemDiv);
-  parentContainer.append(itemContainer);
+  iContainer.append(iSpan, removeButton);
+  return iContainer;
 }
 
-function createIngredientDiv(name, parentContainer){
-  let ingredientDiv = document.createElement('div');
-  ingredientDiv.className = 'ingredientDiv';
-  let ingredientName = document.createElement('span');
-  let removeIngredient = document.createElement('button');
 
-  ingredientName.textContent = name;
-  ingredientDiv.id = name;
-  removeIngredient.textContent = 'remove';
-
-  removeIngredient.addEventListener('click', () => { // slight bug where the parent nodes gets deleted after all the nodes are removed.
-    const parentId = parentContainer.id;
-    // console.log(ingredientDiv.id);
-    removeUserData(parentId, ingredientDiv.id);
-    while(ingredientDiv.firstChild) {
-      ingredientDiv.removeChild(ingredientDiv.firstChild);
-    }
-  });
-
-  ingredientDiv.append(ingredientName, removeIngredient);
-  parentContainer.append(ingredientDiv);
-
-}
-
-addButton.addEventListener('click', () => {
-  const inputVal = itemInput.value;
-  if (inputVal != '' && inputVal != null){
-    const result = writeUserData('Other', inputVal);
-    if (result) {
-      createItemDiv(inputVal, menuContainer);
-    }
-  }
-
-});
-
-addPizzaButton.addEventListener('click', () => {
-  const inputPizza = pizzaInput.value;
-  const key = "Pizza's";
-  if (inputPizza != '' && inputPizza != null){
-    const result = writeUserData(key, inputPizza);
-    if (result) {
-      createPizzaDiv(key, pizzaContainer, inputPizza);
-    }
-  }
-});
-
-
-addPizzMenuButton.addEventListener('click', () => {
-  const inputPizza = pizazMenuInput.value;
-  pizzaInput.value = '';
-  const key = 'PizzaMenu';
-  let newPizza = new Pizza();
-  if (inputPizza != '' && inputPizza != null) {
-    const result = wirtePizzaData(key, pizazMenuInput, newPizza);
-    if (result) {
-      // createPizzaDiv(key, pizzaMenuContainer, inputPizza);
-    }
-  }
-});
-
-
-
-async function showPizzaMenu(startPath, container) {
-  let myData = await readUserData(startPath, '');
-  console.log(myData);
-  for (const key in myData) {
-    // console.log(`Key: ${key}`);
-    createPizzaDiv(startPath, container, key);
-    const keyPath = startPath + '/' + key;
-    let keyDiv = document.getElementById(keyPath); // here is the problem // path name must be given
-    for (const item in myData[key]) {  
-        createItemDiv(item, keyDiv);
-        const itemPath = keyPath + '/' + item;
-        let itemDiv = document.getElementById(itemPath);
-        itemDiv.className = 'itemDiv';
-        // console.log(itemDiv)
-      for (const ingredient in myData[key][item]) {
-        createIngredientDiv(ingredient, itemDiv);
+async function load(path) {
+  const data = await readUserData(path, '');
+  for (const item in data) {
+    // const currentPath = 
+    const curItemDiv = createItem(path, item);
+    ingredientContainer.append(curItemDiv);
+    let nextPath = path + '/' + item;
+    for (const option in data[item]) {
+      let optionPath = nextPath + '/' + option;
+      const optionDiv = createOption(nextPath, option);
+      curItemDiv.append(optionDiv);
+      // console.log(option);
+      for (const ingredient in data[item][option]) {
+        const iDiv = createIngredient(optionPath, ingredient);
+        optionDiv.append(iDiv);
+        // console.log(ingredient);
       }
     }
   }
+  // console.log(data);
 }
 
-async function showCurrentMenu() {
-  let myData = await readUserData('Other', '');
-  console.log(myData);
-  for (const key in myData) {
-    const keyPath = "Other/" + key
-    createItemDiv(key, menuContainer);
-    let keyDiv = document.getElementById(keyPath);
-    for (const ingredient in myData[key]) {
-      // console.log(ingredient);
-      createIngredientDiv(ingredient, keyDiv);
-    }
-  }
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-  showPizzaMenu("Pizza's", pizzaContainer);
-  showPizzaMenu('PizzaMenu', pizzaMenuContainer);
-  showCurrentMenu();
-});
-
-
+addItemButton(ingredientContainer);
+load('testDir');
--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+    import { getDatabase, ref, set, remove, onValue, child, get, update} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
     import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
     //   import { }
@@ -30,3 +31,66 @@
     //   ],
     //   // Other config options...
     // });
+
+
+export async function writeUserData(parentId, childId) {
+  console.log('write ran');
+  console.log(`parentId: ${parentId}, childId: ${childId}`);
+  const db = getDatabase();
+  await set(ref(db, parentId + '/' + childId), '')
+  .then(() => {
+    console.log('data set successfully');
+    return true;
+  })
+  .catch ((error) => {
+    console.error(`Error setting node: ${error}`);
+    return false;
+  });
+}
+
+
+export async function removeUserData(parentId, userId) {
+  console.log('remove ran');
+  console.log(`parentId: ${parentId}, userId: ${userId}`);
+  const db = getDatabase();
+  await remove(ref(db, parentId + '/' + userId))
+  .then(() => {
+    console.log('data removed successfully');
+  })
+  .catch( (error) => {
+    console.error(`Error setting node: ${error}`);
+    return false
+  });
+
+}
+
+export async function readUserData(parentId, childId) { // a single snapshot of the data
+  const dbRef = ref(getDatabase());
+  let data = '';
+  await get(child(dbRef, parentId + '/' + childId)).then((snapshot) => {
+    if (snapshot.exists()){
+      data = snapshot.val();
+    } else {
+      console.log('No data available');
+    }
+  }).catch((error) => {
+    console.error(error);
+    // return false;
+  });
+  // console.log(myMenuItems);
+  return data;
+}
+
+export async function updateUserData(parentPath, child, val) {
+    const db = getDatabase();
+    const parentData = await readUserData(parentPath, '');
+    console.log(parentData[child]);
+    parentData[child] = val;
+    await update(ref(db, parentPath + '/'), parentData)
+    .then(() => {
+        console.log('data updated successfully');
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
